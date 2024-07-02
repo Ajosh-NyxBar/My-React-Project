38b71d48eb51886f898b4f32c2449c39895fd69b
MuhammadRifkyAkbar <2210631170088@student.unsika.ac.id>
2024-07-02 14:31:24 +0700

    add detail

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,11 +1,15 @@
 import "./chat.css";
 import EmojiPicker from "emoji-picker-react";
-import { useState } from "react";
-
+import { useEffect, useState, useRef } from "react";
 const Chat = () => {
   const [open, setOpen] = useState(false);
   const [text, setText] = useState("");
 
+  const endRef = useRef(null);
+
+  useEffect(() => {
+    endRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, []);
   const handleEmoji = (e) => {
     setText((prev) => prev + e.emoji);
     setOpen(false);
@@ -74,16 +78,24 @@ const Chat = () => {
         <div className="message">
           <img src="./avatar.png" alt="" />
           <div className="texts">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, quod? </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
+              quod?{" "}
+            </p>
             <span>1 min ago</span>
           </div>
         </div>
         <div className="message own">
           <div className="texts">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, quod? </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
+              quod?{" "}
+            </p>
             <span>1 min ago</span>
           </div>
         </div>
+
+        <div ref={endRef}></div>
       </div>
       <div className="bottom">
         <div className="icons">
