ff02c9a893eabb4403d0e8447e6ef45fb7d29a4a
MuhammadRifkyAkbar <2210631170088@student.unsika.ac.id>
2024-07-03 16:20:19 +0700

    BackEnd: add addUserList

--- src/components/list/chatList/Chatlist.jsx
+++ src/components/list/chatList/Chatlist.jsx
@@ -1,9 +1,34 @@
+import { doc, onSnapshot } from "firebase/firestore";
+import { useUserStore } from "../../../lib/userStore";
 import Adduser from "./addUser/Adduser";
 import "./chatlist.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { db } from "../../../lib/firebase";
 
 const Chatlist = () => {
+  const [chats, setChats] = useState([]);
   const [addMode, setAddMode] = useState(false);
+
+  const { currentUser } = useUserStore();
+  useEffect(() => {
+    const unSub = onSnapshot(doc(db, "userchats", currentUser.id), async (doc) => {
+      const res = await getDoc(doc);
+      const items = res.data().chats;
+     
+      const promises = items.map(async (chatId) => {
+        const userDocRef = doc(db, "users", item.receiverId);
+        const userDocSnap = await getDoc(userDocRef);
+         
+        const userData = userDocSnap.data();
+        return {...item, user};
+      });
+      const chatData = await Promise.all(promises);
+      setChats(chatData.sort((a, b) => b.updatedAt - a.updatedAt));
+    });
+    return () => {
+      unSub();
+    };
+  }, [currentUser.id]);
 
   return (
     <div className="chatlist">
@@ -19,35 +44,16 @@ const Chatlist = () => {
           onClick={() => setAddMode((prev) => !prev)}
         />
       </div>
-       <div className="item">
-        <img src="./avatar.png" alt="" />
+      {chats.map((chat) => (
+        <div className="item" key={chat.chatId}>
+          <img src="./avatar.png" alt="" />
         <div className="texts">
           <span>Akbar Josh</span>
-          <p>Hello</p>
+          <p>{chat.lastMessage}</p>
+          </div>
         </div>
-       </div>
-       <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Akbar Josh</span>
-          <p>Hello</p>
-        </div>
-       </div>
-       <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Akbar Josh</span>
-          <p>Hello</p>
-        </div>
-       </div>
-       <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Akbar Josh</span>
-          <p>Hello</p>
-        </div>
-       </div>
-       {addMode && <Adduser />}
+      ))}
+      {addMode && <Adduser />}
     </div>
   );
 };
--- src/components/list/chatList/addUser/Adduser.jsx
+++ src/components/list/chatList/addUser/Adduser.jsx
@@ -1,18 +1,43 @@
 import './adduser.css'
+import { useState, useEffect } from 'react';
+import { collection, query, where, getDocs } from 'firebase/firestore';
+import { db } from '../../../../lib/firebase';
 const Adduser = () => {
+
+  const [users, setUsers] = useState(null);
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const username = e.target.username.value;
+    console.log(username);
+
+    try {
+      const userRef = collection(db, "users");
+      const q = query(userRef, where("username", "==", username));
+      const querySnapshot = await getDocs(q);
+      if (!querySnapshot.empty) {
+        setUsers(querySnapshot.docs.map((doc) => doc.data()));
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className='adduser'>
-        <form>
-            <input type="text" name="username" id="" placeholder='Username' />
-            <button>Search</button>
-        </form>
-        <div className="user">
-            <div className="detail">
-                <img src="./avatar.png" alt="" />
-                <span>Akbar Josh</span>
-            </div>
-            <button>Add User</button>
+      <form onSubmit={handleSearch}>
+        <input type="text" name="username" placeholder='Username' />
+        <button>Search</button>
+      </form>
+      {users && users.map((user) => (
+        <div className="user" key={user.id}>
+          <div className="detail">
+            <img src={user.avatar || "./avatar.png"} alt="" />
+            <span>{user.username}</span>
+          </div>
+          <button>Add User</button>
         </div>
+      ))}
     </div>
   );
 }
